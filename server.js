3dcb4a86ec708d2da05f65d710e6e6a0445b620e
Think Dev <=>
2023-09-01 22:02:06 +0100

    add cors for front

--- server.js
+++ server.js
@@ -5,6 +5,11 @@ const upload = require('express-fileupload');
 require('dotenv').config();
 const port = process.env.PORT;
 
+const cors = require('cors');
+
+// apply cors origin
+app.use(cors());
+
 // Application Routes
 const authRoutes = require('./routes/authRoutes');
 const categoryRoutes = require('./routes/categoryRoutes');
@@ -12,6 +17,11 @@ const productRoutes = require('./routes/productRoutes');
 
 app.use(express.json());
 app.use(upload());
+
+app.get('/test',(req,res)=>{
+    res.json({msg:"hello"})
+})
+
 // use Router as middelware
 app.use('/apicbo/auth',authRoutes);
 app.use('/apicbo/category',categoryRoutes);
